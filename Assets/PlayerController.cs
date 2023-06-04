d4a61190c596920af097ed9f0eab9f177fc2b451
adam askari <34635255+bearownage@users.noreply.github.com>
2023-06-04 15:17:08 -0700

    Present disappears on collision

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 1.0f;
+    private float speed = 2.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
---
+++ Assets/PresentController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentController : MonoBehaviour
+{
+
+    // public GameObject present;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Present Controller");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("On Collision Enter: Collision Happening");
+        if (collision.gameObject.name == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Nothing happened");
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collision Happening");
+        if (collision.gameObject.name == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Nothing happened");
+        }
+    }
+}
