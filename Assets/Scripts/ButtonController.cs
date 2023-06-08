54c3ec9a2d834f6e73ec2751a77859dbf56ac0b3
adam askari <34635255+bearownage@users.noreply.github.com>
2023-06-07 17:36:34 -0700

    Added a retry button to the game over screen

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ButtonController : MonoBehaviour
+{
+
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Reload level
+    public void OnClick()
+    {
+        SwitchController.userActivatedSwitch = false;
+        SceneManager.LoadScene("Scenes/Adam's Scene");
+    }
+}
